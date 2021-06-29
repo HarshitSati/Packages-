b941a6eafbd1147734148dade8f4416b87aeaea4
Harshit Sati <59113445+HarshitSati@users.noreply.github.com>
2021-06-29 23:05:50 +0530

    Add files via upload

---
+++ 3a_python_package/distributions/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 29 10:44:40 2021
+
+@author: harsh
+"""
+
+from .gaussiandistribution import Gaussian
---
+++ 3a_python_package/setup.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 29 10:48:12 2021
+
+@author: harsh
+"""
+
+from setuptools import setup
+
+setup(name = 'distributions',version = '0.1', description = 'Gaussian distributions', 
+      packages = ['distributions'], zip_safe = False)
